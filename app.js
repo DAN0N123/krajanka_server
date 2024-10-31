a58462501a815d874126be9f9d7e60a2fba2ea6d
DAN0N123 <filipskubis01@wp.pl>
2024-10-31 20:03:43 +0100

    ???

--- app.js
+++ app.js
@@ -27,6 +27,20 @@ const corsOptions = {
 app.use(cors(corsOptions));
 // app.options("*", cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin",
+    "Content-Type, Authorization"
+  );
+  next();
+});
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
