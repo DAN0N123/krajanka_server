9e1782e6edb9a85ff37f5aedcbbd356dc8aa5a81
DAN0N123 <filipskubis01@wp.pl>
2024-10-31 14:51:58 +0100

    .

--- app.js
+++ app.js
@@ -1,47 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const connection = require('./database');
-const cors = require('cors');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const connection = require("./database");
+const cors = require("cors");
 var app = express();
 // const passport = require('./passport')
 
 //routes
-const productsRouter = require('./routes/productsRouter');
-const clientsRouter = require('./routes/clientsRouter');
-const ordersRouter = require('./routes/ordersRouter');
+const productsRouter = require("./routes/productsRouter");
+const clientsRouter = require("./routes/clientsRouter");
+const ordersRouter = require("./routes/ordersRouter");
 const allowedOrigins = [
-  'http://localhost:5173',
+  "http://localhost:5173",
   process.env.PRODUCTION_ENVIRONMENT,
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
+const corsOptions = {
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
 
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors(corsOptions));
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 // app.use(passport.initialize());
 
-app.use('/products', productsRouter);
-app.use('/clients', clientsRouter);
-app.use('/orders', ordersRouter);
+app.use("/products", productsRouter);
+app.use("/clients", clientsRouter);
+app.use("/orders", ordersRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -51,7 +45,7 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
