2356f84bd8988a647916a4649bc491b6db795732
DAN0N123 <filipskubis01@wp.pl>
2024-10-31 22:27:19 +0100

    .

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-// app.options("*", cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
